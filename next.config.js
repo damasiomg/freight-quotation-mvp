af4fd276285853d93a5029bbd38eab0ff6779540
Diego Damasio <ddamasio@yahoo.com>
2026-03-06 18:23:14 -0300

    chore: remove experimental config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-const path = require('path');
-
-
 const nextConfig = {
   env: {
     API_TOKEN: process.env.API_TOKEN,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-  },
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
   },
   eslint: {
     ignoreDuringBuilds: true,
